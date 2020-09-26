5bb82ac120df124ddfd7ad229b9ac565b86c1c8f
David Rivera <davidrivera62@gmail.com>
2020-09-26 18:26:34 -0500

    Data format correction plot OC analyst Correction

--- R/plotocanalista.R
+++ R/plotocanalista.R
@@ -1,18 +1,19 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 plot.oc.analista <- function(OC, FechaInicio, FechaFin){
 
-  OC %>%
-    filter(FECHA_CIERRE_TAREA > FechaInicio & FECHA_CIERRE_TAREA < FechaFin) %>%
-    filter(ANALISTA_RESPONSANBLE_CIERRE_T != 'David Rivera' | ANALISTA_RESPONSANBLE_CIERRE_T != 'Jaime Gomez' | ANALISTA_RESPONSANBLE_CIERRE_T != 'Yonny Escobar' ) %>%
-    select(ANALISTA_RESPONSANBLE_CIERRE_T,NUM_OC,ESTADO_TAREA) %>%
-    group_by(ANALISTA_RESPONSANBLE_CIERRE_T,ESTADO_TAREA) %>%
+  ocdb %>%
+    filter(FECHA_INICIO_TAREA > FechaInicio & FECHA_CIERRE_TAREA < FechaFin) %>%
+    filter(ANALISTA_RESPONSANBLE_CIERRE_TAREA != 'David Rivera' | ANALISTA_RESPONSANBLE_CIERRE_TAREA != 'Jaime Gomez' | ANALISTA_RESPONSANBLE_CIERRE_TAREA != 'Yonny Escobar' ) %>%
+    select(ANALISTA_RESPONSANBLE_CIERRE_TAREA,NUM_OC,ESTADO_TAREA) %>%
+    group_by(ANALISTA_RESPONSANBLE_CIERRE_TAREA,ESTADO_TAREA) %>%
     summarise(Total = length(!is.na(NUM_OC)))%>%
     ggplot(aes(fill=ESTADO_TAREA,
                y=Total,
-               x=reorder(ANALISTA_RESPONSANBLE_CIERRE_T, Total),
+               x=reorder(ANALISTA_RESPONSANBLE_CIERRE_TAREA, Total),
                label=Total))+
     geom_bar(position = "stack", stat = "identity")+
     scale_fill_brewer(palette = "Paired")+
--- data-raw/create_ocdb.R
+++ data-raw/create_ocdb.R
@@ -1,5 +1,10 @@
 ocdb <- read.csv("./data-raw/OC_DB.csv", check.names = TRUE, sep=";")
 
+ocdb$FECHA_INICIO_TAREA <- as.Date(ocdb$FECHA_INICIO_TAREA,format="%d/%m/%Y %H:%M")
+ocdb$FECHA_CIERRE_TAREA <- as.Date(ocdb$FECHA_CIERRE_TAREA,format="%d/%m/%Y %H:%M")
+ocdb$FECHA_INICIO_TAREA_PROGRAMADA <- as.Date(ocdb$FECHA_INICIO_TAREA_PROGRAMADA,format="%d/%m/%Y %H:%M")
+ocdb$FECHA_CIERRE_TAREA_PROGRAMADA <- as.Date(ocdb$FECHA_CIERRE_TAREA_PROGRAMADA,format="%d/%m/%Y %H:%M")
+
 usethis::use_data(ocdb, overwrite = TRUE)
 
 rm(ocdb)
